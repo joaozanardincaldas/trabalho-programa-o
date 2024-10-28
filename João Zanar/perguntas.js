15d8addec478281b84e3680766a647aad0285a2a
joaozanardincaldas <145141350+joaozanardincaldas@users.noreply.github.com>
2024-10-28 12:06:43 -0300

    Add files via upload

---
+++ João Zanar/perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Quem sou eu?',
+    '',
+    'eu sou o João Zanar, tenho 17 anos e sou fazendeiro'
+)
+
+criaCartao(
+    'Minha familia é?',
+    '',
+    'trabalhadora'
+)
+
+criaCartao(
+    'Onde moro?',
+    '',
+    'Moro em uma chacara no Pinhão, -25.736913, -51.585563'
+)
+
+criaCartao(
+    'Onde estudo?',
+    '',
+    'Estudo no Colegio Estadual Prof. Mario Evaldo Morski fundado em 1987'
+)
+
+
+criaCartao(
+    'Minha turma?',
+    '',
+    'Um bando de vadios'
+)
+
+
+criaCartao(
+    'Faculdade?',
+    '',
+    'Planejo me formar em Agronomia'
+)
+
+
+criaCartao(
+    'Minha cidade?',
+    '',
+    'Uma cidade pouco desenvolvida, mas sossegada'
+)
+
+
+criaCartao(
+    'Meu sonho?',
+    '',
+    'Ficar rico '
+)
